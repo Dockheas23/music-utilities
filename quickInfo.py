731f021d4c1c8bf643dc7b8e8837493785405a21
George Boyle <george@thebuds.net>
2010-06-25 15:25:41 +0100

    Added quickInfo.py file and README message

---
+++ quickInfo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import sys
+from xml.dom import minidom
+
+xmlDoc = minidom.getDOMImplementation().createDocument(None, 'disc', None)
+rootNode = xmlDoc.documentElement
+
+for tagName in ['artist', 'album', 'genre', 'date', 'discnumber']:
+    print('Enter', tagName + ': ', file=sys.stderr)
+    tagValue = sys.stdin.readline().strip()
+    if tagValue != "":
+        newNode = rootNode.appendChild(xmlDoc.createElement(tagName))
+        newNode.appendChild(xmlDoc.createTextNode(tagValue))
+
+print('Enter each track name and press return (Ctrl-D when done)',
+        file=sys.stderr)
+tracks = sys.stdin.readlines()
+for i in range(len(tracks)):
+    trackTag = rootNode.appendChild(xmlDoc.createElement('track'))
+    titleTag = trackTag.appendChild(xmlDoc.createElement('title'))
+    trackNumTag = trackTag.appendChild(xmlDoc.createElement('tracknumber'))
+    trackNumTag.appendChild(xmlDoc.createTextNode(str(i + 1)))
+    titleTag.appendChild(xmlDoc.createTextNode(tracks[i].strip()))
+
+xmlDoc.writexml(sys.stdout, encoding='utf-8')
