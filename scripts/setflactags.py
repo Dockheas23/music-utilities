7e9a25436dfd071bf4fa1d31edd3fe35f5ec3e94
George Boyle <george@thebuds.net>
2013-01-05 18:51:05 +0000

    Fixed bug where multiple vorbis comment tags were being overwritten by the last one

--- scripts/setflactags.py
+++ scripts/setflactags.py
@@ -3,28 +3,39 @@
 import os, sys, re
 from xml.etree import ElementTree
 
+def get_tags(parent_node):
+    result = {}
+    for node in parent_node.getchildren():
+        if node.tag != 'track':
+            if node.tag not in result:
+                result[node.tag] = [node.text]
+            else:
+                result[node.tag].append(node.text)
+    return result
+
 tree = ElementTree.parse('info.xml')
 root = tree.getroot()
 
 tracks = tree.findall('track')
-albumTags = {node.tag: node.text for node in root.getchildren()
-        if node.tag != 'track'}
+albumTags = get_tags(root)
 if not 'artist' in albumTags:
-    albumTags['artist'] = 'Various'
+    albumTags['artist'] = ['Various']
 albumTags['albumartist'] = albumTags['artist']
 
 for arg in sys.argv[1:]:
     trackNum = re.match(r'\d{2}', arg)
     track = tracks[int(trackNum.group(0)) - 1]
-    trackTags = {node.tag: node.text for node in track.getchildren()}
+    trackTags = get_tags(track)
     options = ""
     tagsAdded = {}
-    for tagName, tagValue in trackTags.items():
-        tagString = tagName.upper() + '=' + tagValue.strip()
-        options += '--set-tag="' + tagString + '" '
-        tagsAdded[tagName] = 1
-    for tagName, tagValue in albumTags.items():
-        if tagName not in tagsAdded:
+    for tagName, tagValues in trackTags.items():
+        for tagValue in tagValues:
             tagString = tagName.upper() + '=' + tagValue.strip()
             options += '--set-tag="' + tagString + '" '
+            tagsAdded[tagName] = 1
+    for tagName, tagValues in albumTags.items():
+        for tagValue in tagValues:
+            if tagName not in tagsAdded:
+                tagString = tagName.upper() + '=' + tagValue.strip()
+                options += '--set-tag="' + tagString + '" '
     os.system('metaflac ' + options + arg)
